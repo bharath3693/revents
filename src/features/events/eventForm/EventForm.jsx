401de12e0e1edd4da3fd8e8667019db162455fbb
bharath3693 <saibharath3693@gmail.com>
2021-02-03 21:08:23 +0530

    End Of Section 5

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,58 +1,110 @@
-import cuid from 'cuid';
-import React, { useState } from 'react'; 
-import { Button, Form, Header, Segment } from 'semantic-ui-react';
+import cuid from "cuid";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Form, Header, Segment } from "semantic-ui-react";
 
-export default function EventForm({setFormOpen, setEvents, createEvent, selectedEvent, updateEvent}){
-    const initialValues = selectedEvent ??  {
-        title : '',
-        category : '',
-        description : '',
-        city : '',
-        venue : '',
-        date : ''
-    }
-    const [values, setValues] = useState(initialValues);
+export default function EventForm({
+  setFormOpen,
+  setEvents,
+  createEvent,
+  selectedEvent,
+  updateEvent,
+}) {
+  const initialValues = selectedEvent ?? {
+    title: "",
+    category: "",
+    description: "",
+    city: "",
+    venue: "",
+    date: "",
+  };
+  const [values, setValues] = useState(initialValues);
 
-    function handleFormSubmit(){
-        selectedEvent 
-        ? updateEvent({...selectedEvent, ...values}) : 
-        createEvent({...values,id: cuid(),hostedBy: 'Bob', attendees : [],hostPhotoURL : '/assets/user.png'})
-        setFormOpen(false)
-    }
+  function handleFormSubmit() {
+    selectedEvent
+      ? updateEvent({ ...selectedEvent, ...values })
+      : createEvent({
+          ...values,
+          id: cuid(),
+          hostedBy: "Bob",
+          attendees: [],
+          hostPhotoURL: "/assets/user.png",
+        });
+    setFormOpen(false);
+  }
 
-    function handleInputChange(e){
-        const {name, value} = e.target;
-        setValues({...values, [name]:value})
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  }
 
-    }
-
-    return (
-        <Segment clearing>
-            <Header content={selectedEvent ? "Edit the Event" : "Create new Event"}/>
-            <Form onSubmit={handleFormSubmit}>
-                <Form.Field>
-                    <input type="text" name='title' placeholder="Event title" value={values.title} onChange={(e) => handleInputChange(e)}/>
-                </Form.Field>
-                <Form.Field>
-                    <input type="text" name='category' placeholder="Category" value={values.category} onChange={(e) => handleInputChange(e)}/>
-                </Form.Field>
-                <Form.Field>
-                    <input type="text" name='description' placeholder="Description" value={values.description} onChange={(e) => handleInputChange(e)}/>
-                </Form.Field>
-                <Form.Field>
-                    <input type="text" name='city' placeholder="City" value={values.city} onChange={(e) => handleInputChange(e)}/>
-                </Form.Field>
-                <Form.Field>
-                    <input type="text" name='venue' placeholder="Venue" value={values.venue} onChange={(e) => handleInputChange(e)}/>
-                </Form.Field>
-                <Form.Field>
-                    <input type="date" name='date' placeholder="Date" value={values.date} onChange={(e) => handleInputChange(e)}/>
-                </Form.Field>
-                <Button type='submit' floated='right' positive content="submit"/>
-                <Button type='submit' onClick={() => setFormOpen(false)} floated='right' content="Cancel"/>
-            </Form>
-
-            
-        </Segment>
-    )
+  return (
+    <Segment clearing>
+      <Header content={selectedEvent ? "Edit the Event" : "Create new Event"} />
+      <Form onSubmit={handleFormSubmit}>
+        <Form.Field>
+          <input
+            type="text"
+            name="title"
+            placeholder="Event title"
+            value={values.title}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            type="text"
+            name="category"
+            placeholder="Category"
+            value={values.category}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={values.description}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            type="text"
+            name="city"
+            placeholder="City"
+            value={values.city}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            type="text"
+            name="venue"
+            placeholder="Venue"
+            value={values.venue}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            type="date"
+            name="date"
+            placeholder="Date"
+            value={values.date}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </Form.Field>
+        <Button type="submit" floated="right" positive content="submit" />
+        <Button
+          as={Link}
+          to="/events"
+          type="submit"
+          floated="right"
+          content="Cancel"
+        />
+      </Form>
+    </Segment>
+  );
 }
--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import { Button, Container, Menu } from 'semantic-ui-react';
+import React, { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { Button, Container, Menu } from "semantic-ui-react";
+import SignedInMenu from "./SignedInMenu";
+import SignedOutMenu from "./signedOutMenu";
 
-export default function NavBar({setFormOpen}){
-    return (
-        <Menu inverted fixed="top">
-            <Container>
-                <Menu.Item header>
-                    <img src="https://learning.shareappscrack.com/assets/images/logo.png" alt="logo" style={{marginRight:15}}/>
-                    Re-vents
-                </Menu.Item>
-                <Menu.Item name="Events"></Menu.Item> 
-                <Menu.Item>
-                    <Button onClick={() => setFormOpen(true)} positive inverted content="Create event"/>
-                </Menu.Item>
-                <Menu.Item position="right">
-                    <Button basic inverted content="Login"/>
-                    <Button basic inverted content="Register" style={{marginLeft : '0.5em'}}/>
-                </Menu.Item>
-            </Container>
-        </Menu>
-    )
+export default function NavBar({ setFormOpen }) {
+  const history = useHistory();
+  const [authenticated, setAuthenticated] = useState(false);
+
+  function handleSignOut() {
+    setAuthenticated(false);
+    history.push("/");
+  }
+
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item as={NavLink} exact to="/" header>
+          <img
+            src="https://learning.shareappscrack.com/assets/images/logo.png"
+            alt="logo"
+            style={{ marginRight: 15 }}
+          />
+          Re-vents
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/events" name="Events"></Menu.Item>
+        {authenticated && (
+          <Menu.Item as={NavLink} to="/createEvent">
+            <Button positive inverted content="Create event" />
+          </Menu.Item>
+        )}
+
+        {authenticated ? (
+          <SignedInMenu SignOut={handleSignOut} />
+        ) : (
+          <SignedOutMenu setAuthenticated={setAuthenticated} />
+        )}
+      </Container>
+    </Menu>
+  );
 }
